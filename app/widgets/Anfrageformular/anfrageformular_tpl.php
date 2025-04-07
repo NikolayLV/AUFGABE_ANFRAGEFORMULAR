555525a638d260c4cc98fe34ab279878466f91cc
Nikolay <Nikolayd2002@gmail.com>
2025-04-07 21:07:36 +0200

    Text edits

--- app/widgets/Anfrageformular/anfrageformular_tpl.php
+++ app/widgets/Anfrageformular/anfrageformular_tpl.php
@@ -14,7 +14,7 @@
 						'required' => true,
 						'maxlength' => 50
 					]);
-					echo $this->getTooltip($id = 'reisezeitraum', $tooltipText = 'Gegeben Sie einen Reisedatum ein.');
+					echo $this->getTooltip($id = 'reisezeitraum', $tooltipText = 'Geben Sie ein Reisedatum ein.');
 				?>
 				<script>
 					/**
@@ -43,8 +43,7 @@
 						'id' => 'anzahl_erwachsene',
 						'class' => 'default_input',
 						'required' => true,
-						'maxlength' => 50,
-						'data-tooltip' => 'Fülle dieses Feld aus'
+						'maxlength' => 50
 					]);
 					echo $this->getTooltip($id = 'anzahl_erwachsene', $tooltipText = 'Gegeben Sie eine Positiv Zahl ein.');
 				?>
@@ -66,8 +65,7 @@
 							'id' => 'name_kindes',
 							'class' => 'default_input',
 							'required' => true,
-							'maxlength' => 50,
-							'data-tooltip' => 'Fülle dieses Feld aus'
+							'maxlength' => 50
 						]);
 						echo $this->getTooltip($id = 'name_kindes', $tooltipText = 'Gegeben Sie einen gültigen Namen ein.');
 					?>
@@ -127,14 +125,14 @@
 				<p class="form_bodytext">Vorname *</p>
 				<?php
 					echo $this->getTextInput($id = 'vorname', $name = 'Vorname');
-					echo $this->getTooltip($id = 'vorname', $tooltipText = 'Gegeben Sie eine gültige Vorname ein.');
+					echo $this->getTooltip($id = 'vorname', $tooltipText = 'Gegeben Sie einen gültigen Vorname ein.');
 				?>
 			</div>
 			<div class="input_section">
 				<p class="form_bodytext">Nachname *</p>
 				<?php
 					echo $this->getTextInput($id = 'nachname', $name = 'Nachname',);
-					echo $this->getTooltip($id = 'nachname', $tooltipText = 'Gegeben Sie eine gültige Nachname ein.');
+					echo $this->getTooltip($id = 'nachname', $tooltipText = 'Gegeben Sie einen gültigen Nachname ein.');
 				?>
 			</div>
 			<div class="input_section">
@@ -153,29 +151,28 @@
 					];
 
 					echo $this->getDropdownList($attributes, $options);
-					echo $this->getTooltip($id = 'geschlecht', $tooltipText = 'Wählen Sie eine Geschlecht aus.');
+					echo $this->getTooltip($id = 'geschlecht', $tooltipText = 'Wählen Sie ein Geschlecht aus.');
 				?>
 			</div>
 			<div class="input_section">
-				<p class="form_bodytext">Email *</p>
+				<p class="form_bodytext">E-mail *</p>
 				<?php
 					echo $this->getInput([
 						'type' => 'Email',
-						'name' => 'Email',
+						'name' => 'E-mail',
 						'id' => 'email',
 						'class' => 'default_input',
 						'required' => true,
-						'maxlength' => 50,
-						'data-tooltip' => 'Fülle dieses Feld aus'
+						'maxlength' => 50
 					]);
-					echo $this->getTooltip($id = 'email', $tooltipText = 'Gegeben Sie eine gültige Email ein.');
+					echo $this->getTooltip($id = 'email', $tooltipText = 'Geben Sie eine gültige E-Mail-Adresse ein.');
 				?>
 			</div>
 			<div class="input_section">
 				<p class="form_bodytext">Land *</p>
 				<?php
 					echo $this->getCountriesDropdownList();
-					echo $this->getTooltip($id = 'land', $tooltipText = 'Wählen Sie eine Land aus.');
+					echo $this->getTooltip($id = 'land', $tooltipText = 'Wählen Sie ein Land aus.');
 				?>
 			</div>
 			<div class="input_section">
@@ -187,24 +184,23 @@
 						'id' => 'plz',
 						'class' => 'default_input',
 						'required' => true,
-						'maxlength' => 5,
-						'data-tooltip' => 'Fülle dieses Feld aus'
+						'maxlength' => 5
 					]);
-					echo $this->getTooltip($id = 'plz', $tooltipText = 'Gegeben Sie einen gültigen PLZ ein.');
+					echo $this->getTooltip($id = 'plz', $tooltipText = 'Geben Sie eine gültige Postleitzahl ein.');
 				?>
 			</div>
 			<div class="input_section">
 				<p class="form_bodytext">Stadt *</p>
 				<?php
 					echo $this->getTextInput($id = 'stadt', $name = 'Stadt');
-					echo $this->getTooltip($id = 'stadt', $tooltipText = 'Gegeben Sie eine gültige Stadt ein.');
+					echo $this->getTooltip($id = 'stadt', $tooltipText = 'Geben Sie eine gültige Stadt ein.');
 				?>
 			</div>
 			<div class="input_section">
 				<p class="form_bodytext thin">Straße *</p>
 				<?php
 					echo $this->getTextInput($id = 'strasse', $name = 'Strasse');
-					echo $this->getTooltip($id = 'strasse', $tooltipText = 'Gegeben Sie eine gültige Straße ein.');
+					echo $this->getTooltip($id = 'strasse', $tooltipText = 'Geben Sie eine gültige Straße ein.');
 				?>
 			</div>
 			<div class="input_section">
@@ -215,8 +211,7 @@
 						'name' => 'Telefon',
 						'id' => 'telefon',
 						'class' => 'default_input',
-						'maxlength' => 14,
-						'data-tooltip' => 'Fülle dieses Feld aus'
+						'maxlength' => 14
 					]);
 
 				?>
